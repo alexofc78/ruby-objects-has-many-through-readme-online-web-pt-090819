dbf185e7c0981c14e565a78f1a6604d0d4734ee0
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-10-22 14:57:13 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,23 @@
 class Meal
+  
+    
+   attr_accessor :waiter, :costumer, :total, :tip
+  
+  @@all = []
+  
+  def initialize (waiter, costumer, total, tip=0)
+    @waiter = waiter
+    @costumer = costumer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  
+  
 
 end
